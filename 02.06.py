7b9c33ae80d291025a31a34ebbd11cb461adfeef
Взрослая академия <georgygevorkov@gmail.com>
2026-06-02 21:17:10 +0300

    условный оператор

---
+++ 02.06.py
@@ -0,0 +1,15 @@
+# разложение числа на разряды
+# num1 = int(input("Введите число: "))
+# d1 = num1%10
+# d2 = num1//10%10
+# d3 = num1//100%10
+# d4 = num1//1000
+# print((d1*1000+d2*100+d3*10
+
+name = (input("введите имя: "))
+if name=="Ivan"
+    print("good")
+elif name=="ivan"
+    print("good")
+else:
+    print("Вы ввели не имя")
