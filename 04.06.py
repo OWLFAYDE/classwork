151100a18d1bc4b66297eacb3573c3a3c194f415
zalesskiy <georgygevorkov@gmail.com>
2026-06-04 21:18:39 +0300

    условный оператор

---
+++ 04.06.py
@@ -0,0 +1,26 @@
+#1
+# num1=int(input("введите число: "))
+# num2=int(input("введите число: "))
+# num3=int(input("введите число: "))
+# if num1>num2:
+#     if num1>num3:
+#         print(num1)
+#     else:
+#         print(num3)
+# elif num2>num3:
+#     print(num2)
+# else:
+#     print(num3)
+
+# num1 = int(input("введите число: "))
+# if num1<10 and num1>0:
+#     print("win")
+# elif num1<0:
+#     print("lose")
+# else:
+#     print("lose")
+
+
+num1 = int(input("введите число: "))
+if num1<=0 or num1>=10:
+    print("win")
