49cf3ebe59d89893a96b552178773eebf49a3da3
zalesskiy <georgygevorkov@gmail.com>
2026-06-04 21:21:19 +0300

    логический оператор

--- 02.06.py
+++ 02.06.py
@@ -6,10 +6,10 @@
 # d4 = num1//1000
 # print((d1*1000+d2*100+d3*10
 
-name = (input("введите имя: "))
-if name=="Ivan"
-    print("good")
-elif name=="ivan"
-    print("good")
-else:
-    print("Вы ввели не имя")
+# name = (input("введите имя: "))
+# if name== "Ivan"
+#      print("good")
+# elif name== "ivan"
+#      print("good")
+# else:
+#     print("Вы ввели не имя")
