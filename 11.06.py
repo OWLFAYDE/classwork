c28465c7593d03a1dfcbf36e4daf8338e0fb0ebe
zalesskiy <georgygevorkov@gmail.com>
2026-06-11 21:08:42 +0300

    цикл for

---
+++ 11.06.py
@@ -0,0 +1,30 @@
+# number = int(input("введите число: "))
+# print(number % 2 and "нечетное" or "четное")
+
+# a = int(input(""))
+# b = int(input(""))
+# if a<b: a,b=b,a
+# for i in range (a,b+1,1):
+#     print(i, end=" ")
+
+# a = int(input(""))
+# b = int(input(""))
+# mult = 1
+# if a>b: a,b=b,a
+# for i in range(a,b+1):
+#     mult*=i
+#     print(mult)
+
+# num = int(input())
+# maxs = num
+# for i in range(4):
+#     num = int(input())
+#     if maxs < num:
+#         maxs = num
+# print(maxs)
+
+# n = int(input())
+# for i in range (n):
+#     num = int(input())
+#     if num%2==0:
+#         print(num, end=" ")
