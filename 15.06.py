64dc95452c10bee1d25346886074c60f3f60a63b
zalesskiy <georgygevorkov@gmail.com>
2026-06-15 21:19:59 +0300

    цикл for

---
+++ 15.06.py
@@ -0,0 +1,48 @@
+# a = int(input(" "))
+# b = int(input(" "))
+# summa = 0
+# for i in range (a,b+1):
+#     print(i, end="")
+# count = int(input(" "))
+# row = int(input(" "))
+# for i in range(1, row+1):
+#     for j in range(1, count+1):
+#         print(f"{i} : {j}", end=" ")
+# #     print()
+# n = int(input(" "))
+# for i in range(n):
+#     print("* "*(i+1))
+#
+# n = int(input(" "))
+# for i in range(n,-1,-1):
+#     print("* "*(i))
+#
+# n = int(input(" "))
+# for i in range(n,-1,-1):
+#     print("  "*(n-i)+"* "*(i))
+
+# n = int(input(" "))
+# for i in range(n):
+#     print("  "*(n-i)+"* "*(i+1))
+
+# n = int(input(" "))
+# print("* " * n)
+# for i in range(n-2):
+#     print("* "+"  "*(n-2) + "* ")
+# print("* " * n)
+
+n = int(input(" "))
+for i in range(n+1):
+    print(" "*(n-i)+"* "*(i))
+#
+# n = int(input(" "))
+# for i in range(n,-1,-1):
+#     print(" "*(n-i)+"* "*(i))
+
+# n = int(input(" "))
+# for i in range((n+1)//2):
+#     print("  "*i+"* "*(n-i*2))
+
+# n = int(input(" "))
+# for i in range((n-1)//2):
+#     print(" "*(n+i//2))
